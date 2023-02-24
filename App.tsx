1024c0864b771caa70232b69d256aee56f8ec31a
Rafael Araujo <rafa123839@gmail.com>
2023-02-24 17:17:31 -0300

    Projeto iniciado

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Dashboard } from './src/screens/Dashboard';
+
+export default function App() {
+  return (
+    <Dashboard/>
+  );
+}
+

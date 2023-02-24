c3c1e90c7926df102edef959c80ac8837ac6a8a0
Rafael Araujo <rafa123839@gmail.com>
2023-02-24 18:09:48 -0300

    adicionado styled compents

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+
+import {Container, Title} from './styles'
 
 export function Dashboard() {
     return (
-        <View style={styles.container}>
-            <Text>Dashboard</Text>
-        </View>
+        <Container>
+            <Title>Dashbsoard</Title>
+        </Container>
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})

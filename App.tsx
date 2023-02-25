f66ec5cdbdb050e03529b8020015fff00d8bffc8
Rafael Araujo <rafa123839@gmail.com>
2023-02-25 08:21:49 -0300

    adicionado fonts personalizadas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
-import {ThemeProvider} from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 import { Dashboard } from './src/screens/Dashboard';
 import theme from './src/global/styles/theme';
-
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  })
+
+  if(!fontsLoaded) {
+    return <AppLoading/>
+  }
+
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard/>
+      <Dashboard />
     </ThemeProvider>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native'
+import theme from '../../global/styles/theme'
 
 export const Container = styled.View`
     flex: 1;
@@ -9,6 +10,7 @@ export const Container = styled.View`
 `
 
 export const Title = styled.Text`
+font-family: ${({theme}) => theme.fonts.regular};
     font-size: 24px;
     font-weight: bold;
     color: ${({theme}) => theme.colors.title};

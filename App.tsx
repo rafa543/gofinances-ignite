0b2bfca20ecd3b0b69d828424de67a8fa0691d53
Rafael Araujo <rafa123839@gmail.com>
2023-02-25 08:10:10 -0300

    adicionando estilos globais

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {ThemeProvider} from 'styled-components'
 import { Dashboard } from './src/screens/Dashboard';
+import theme from './src/global/styles/theme';
 
 export default function App() {
   return (
-    <Dashboard/>
+    <ThemeProvider theme={theme}>
+      <Dashboard/>
+    </ThemeProvider>
   );
 }
 
